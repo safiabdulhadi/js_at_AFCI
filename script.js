f468bacea19a24256c0f1d1d5852c85aaa0f34fc
safiabdulhadi <safiabdulhadi1994@gmail.com>
2022-06-28 17:00:31 +0200

    trying to understand the fun

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
-var a = ["ali", "khan", "nawid", "abdul"];
-var b =a.slice(-3);
-console.log(b)
+// var a = ["ali", "khan", "nawid", "abdul"];
+// var b =a.slice(-3);
+// console.log(b)
+
+// if statements 
+
+// const time = 19;
+
+// if(time < 12) {
+//     console.log('Good morning');
+// }
+// else if(time === 12){
+//     console.log('oh it is noon!');
+// } else if(time > 12 && time<18){
+//     console.log('Good afternoon')
+// }
+// else {
+//     console.log('good evening');
+// }
+
+
+// Comparison operators
+// let x = 20;
+// // Equal
+// console.log(x ==20)
+// console.log(x =='20')
+// // Not Equal 
+// console.log(x !=20) //false
+// console.log(x !=2); //This is true
+// // Deeply Equal
+// console.log(x ===20); //This is true
+// console.log(x ==='20'); //this will be false coz its string
+
+// // Strict not Equal
+// console.log(x !==20); //this is false
+// console.log(x !=="20"); //This is true
+// console.log(x !==2);//This is true coz it not the same value
+
+// Greater than
+
+// Greater or equal 
+
+// les than
+
+// les than or equal
+
+
+// // Logical Operators
+// const fruit = ["apple", "banana", 'grape'];
+
+// // And
+// if(fruit.includes('apple') && fruit.includes('banana')){
+//     console.log('Both fruits exist its mean that are true');
+// }
+
+// //or
+// if(fruit.includes('coconut')|| fruit.includes('apple')){
+//     console.log('Atleast one fruit exists');
+// }
+
+// // Not
+// if(!fruit.includes('cherry')){
+//     console.log('Cherry is missing')
+// }
+
+// let divisible = 1;
+
+// if (divisible == 1) {
+//     console.log('one')
+// } else if (disible == 5) {
+//     console.log("five")
+// } else if (disible == 15) {
+//     console.log(15)
+// }else{
+//     console.log('out')
+// }
+let num = 15;
+
+if(num % 15 ===0 ){
+    console.log('fizz  buzz')
+} else if (num % 5=== 0){
+    console.log('buzz')
+} else if (num % 3=== 0){
+    console.log('fizz')
+} else {
+    console.log(num)
+}
