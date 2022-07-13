375f0d09952df2b73a2e6cdd779c5e6813f52653
safiabdulhadi <safiabdulhadi1994@gmail.com>
2022-07-13 16:57:02 +0200

    destrucuing array

---
+++ destructuing_Array/script.js
@@ -0,0 +1,37 @@
+"use strict";
+// let user = ["yahoo baba", 25, "Lille", [
+//     "male", 2500
+// ]];
+
+// console.log(user);
+// let [name, age, city, [gender, salary]] = user;
+
+// let user = ["yahoo baba", 25, "Lille"];
+
+// let [name, ... args] = user;
+
+// console.log(name);
+// console.log(args);
+// console.log(age);
+// console.log(city); 
+// console.log(gender);
+// console.log(salary);
+
+
+
+// function user([name, age = 28, city]){
+//     console.log(name);
+//     console.log(age);
+//     console.log(city);
+// }
+// user(["yahoo baba", 25, "Lille"]);
+
+
+function user(){
+ return ["yahoo baba", 25, "Lille"];
+}
+
+let [name, age = 28, city] = user();
+console.log(name);
+console.log(age);
+console.log(city);
